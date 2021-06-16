689249bf0a820b34b08eb3b2c3fbf86dc288993c
Aji Manarul Aziz <ajimanarul@gmail.com>
2021-06-16 08:50:05 +0700

    read csv not fix

---
+++ modul.py
@@ -0,0 +1,17 @@
+import csv
+
+def kelas():
+    with open('kelasc.csv', 'r', encoding = 'utf-8-sig') as csvfile:
+        csvreader = csv.reader(csvfile)
+
+def quiz1():
+    with open('quiz1.csv', 'r', encoding = 'utf-8-sig') as csvfile:
+        csvreader = csv.reader(csvfile)
+
+def quiz2():
+    with open('quiz2.csv', 'r', encoding = 'utf-8-sig') as csvfile:
+        csvreader = csv.reader(csvfile)
+
+def quiz3():
+    with open('quiz3.csv', 'r', encoding = 'utf-8-sig') as csvfile:
+        csvreader = csv.reader(csvfile)
