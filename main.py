5f0cdf54317a7eb0d13cf2979f5a007229f9c322
Aji Manarul Aziz <ajimanarul@gmail.com>
2021-06-16 15:23:15 +0700

    no 6 7

---
+++ main.py
@@ -0,0 +1,113 @@
+import sort
+from modul import *
+header_kelas.extend(["Quiz 1, Quiz 2, Quiz 3"])
+## no 7
+### Quiz 1
+for i in nilai_quiz1:
+    i[0] = int(i[0])
+    i[1] = float(i[1])
+
+sort.selection(nilai_quiz1,0)
+
+i = 39
+n = 0
+while i <= len(nilai_quiz1):
+    if n >= len(data_kelas):
+        break
+    if nilai_quiz1[i][0] == int(data_kelas[n][0][5:]):
+        data_kelas[n].append(nilai_quiz1[i][1])
+        n += 1
+        i += 1
+    else:
+        for j in range(i,len(nilai_quiz1)):
+            if nilai_quiz1[j][0] == int(data_kelas[n][0][5:]):
+                data_kelas[n].append(nilai_quiz1[j][1])
+                break
+        data_kelas[n].append(0)
+        if len(data_kelas[n]) == 4:
+            data_kelas[n].pop(-1)
+        n += 1
+
+### Quiz 2
+for i in nilai_quiz2:
+    i[0] = int(i[0][5:])
+    i[1] = float(i[1])
+
+sort.selection(nilai_quiz2,0)
+
+i = 37
+n = 0
+while i <= len(nilai_quiz2):
+    if n >= len(data_kelas):
+        break
+    if nilai_quiz2[i][0] == int(data_kelas[n][0][5:]):
+        data_kelas[n].append(nilai_quiz2[i][1])
+        n += 1
+        i += 1
+    else:
+        for j in range(i,len(nilai_quiz2)):
+            if nilai_quiz2[j][0] == int(data_kelas[n][0][5:]):
+                data_kelas[n].append(nilai_quiz2[j][1])
+                break
+        data_kelas[n].append(0)
+        if len(data_kelas[n]) == 5:
+            data_kelas[n].pop(-1)
+        n += 1
+
+### Quiz 3
+for i in nilai_quiz3:
+    i[0] = float(i[0])
+    i[1] = int(i[1][5:])
+
+sort.selection(nilai_quiz3,1)
+
+i = 37
+n = 0
+while i <= len(nilai_quiz3):
+    if n >= len(data_kelas):
+        break
+    if nilai_quiz3[i][1] == int(data_kelas[n][0][5:]):
+        data_kelas[n].append(nilai_quiz3[i][0])
+        n += 1
+        i += 1
+    else:
+        for j in range(i,len(nilai_quiz3)):
+            if nilai_quiz3[j][1] == int(data_kelas[n][0][5:]):
+                data_kelas[n].append(nilai_quiz3[j][0])
+                break
+        data_kelas[n].append(0)
+        if len(data_kelas[n]) == 6:
+            data_kelas[n].pop(-1)
+        n += 1
+
+print("="*75)
+print("Data Nilai (No. 7)".center(75))
+print("="*75)
+
+header_kelas.extend(["Quiz 1","Quiz 2","Quiz 3"])
+
+for i in header_kelas:
+    print(i, end="\t")
+print("")
+
+data_Quiz = []
+for i in data_kelas:
+    data_Quiz.append(i)
+
+for i in data_Quiz[0]:
+    print(i, end="\t")
+print("")
+for i in data_Quiz[1]:
+    print(i, end="\t")
+print("")
+for i in data_Quiz[2]:
+    print(i, end="\t")
+print("")
+for i in data_Quiz[-2]:
+    print(i, end="\t")
+print("")
+for i in data_Quiz[-1]:
+    print(i, end="\t")
+print("")
+print("-"*75)
+print("")
--- modul.py
+++ modul.py
@@ -1,17 +1,137 @@
 import csv
 
+data_kelas = []
+nilai_quiz1 = []
+nilai_quiz2 = []
+nilai_quiz3 = []
+header_kelas = []
 def kelas():
     with open('kelasc.csv', 'r', encoding = 'utf-8-sig') as csvfile:
         csvreader = csv.reader(csvfile)
+        print("")
+        print("Siswa Kelas C".center(36))
+        print('-'*36)
+        headers = next(csvreader)
+        header_kelas.append(headers)
+        for i in headers:
+            print(i, end="\t\t")
+        for siswa_c in csvreader:
+            data_kelas.append(siswa_c)   
+        print("")
+        for siswa in data_kelas[0]:
+            print(siswa, end="|")
+        print("")
+        for siswa in data_kelas[1]:
+            print(siswa, end="|")
+        print("")
+        for siswa in data_kelas[2]:
+            print(siswa, end="|")
+        print("")
+        for siswa in data_kelas[-2]:
+            print(siswa, end="|")
+        print("")
+        for siswa in data_kelas[-1]:
+            print(siswa, end="|")
+        print("")
 
 def quiz1():
     with open('quiz1.csv', 'r', encoding = 'utf-8-sig') as csvfile:
         csvreader = csv.reader(csvfile)
+        
+        print("")
+        print("Nilai Quiz 1".center(36))
+        print('-'*36)
+        
+        headers = next(csvreader)
+        for a in headers:
+            print(a, end="\t")
+        
+        for quiz1 in csvreader:
+            nilai_quiz1.append(quiz1)   
+        print("")
+
+        for quiz1 in nilai_quiz1[0]:
+            print(quiz1, end="\t|")
+        print("")
+        for quiz1 in nilai_quiz1[1]:
+            print(quiz1, end="\t|")
+        print("")
+        for quiz1 in nilai_quiz1[2]:
+            print(quiz1, end="\t|")
+        print("")
+        for quiz1 in nilai_quiz1[-2]:
+            print(quiz1, end="\t|")
+        print("")
+        for siswa in nilai_quiz1[-1]:
+            print(siswa, end="\t|")
+        print("")
 
 def quiz2():
     with open('quiz2.csv', 'r', encoding = 'utf-8-sig') as csvfile:
         csvreader = csv.reader(csvfile)
+        
+        print("")
+        print("Nilai Quiz 2".center(36))
+        print('-'*36)
+        
+        headers = next(csvreader)
+        for a in headers:
+            print(a, end="\t\t")
+        
+        for quiz2 in csvreader:
+            nilai_quiz2.append(quiz2)   
+        print("")
+
+        for quiz2 in nilai_quiz2[0]:
+            print(quiz2, end="\t|")
+        print("")
+        for quiz2 in nilai_quiz2[1]:
+            print(quiz2, end="\t|")
+        print("")
+        for quiz2 in nilai_quiz2[2]:
+            print(quiz2, end="\t|")
+        print("")
+        for quiz2 in nilai_quiz2[-2]:
+            print(quiz2, end="\t|")
+        print("")
+        for quiz2 in nilai_quiz2[-1]:
+            print(quiz2, end="\t|")
+        print("")
 
 def quiz3():
     with open('quiz3.csv', 'r', encoding = 'utf-8-sig') as csvfile:
         csvreader = csv.reader(csvfile)
+        
+        print("")
+        print("Nilai Quiz 3".center(36))
+        print('-'*36)
+        
+        headers = next(csvreader)
+        for a in headers:
+            print(a, end="\t\t")
+        
+        for quiz3 in csvreader:
+            nilai_quiz3.append(quiz3)   
+        print("")
+
+        for quiz3 in nilai_quiz3[0]:
+            print(quiz3, end="\t|")
+        print("")
+        for quiz3 in nilai_quiz3[1]:
+            print(quiz3, end="\t|")
+        print("")
+        for quiz3 in nilai_quiz3[2]:
+            print(quiz3, end="\t|")
+        print("")
+        for quiz3 in nilai_quiz3[-2]:
+            print(quiz3, end="\t|")
+        print("")
+        for quiz3 in nilai_quiz3[-1]:
+            print(quiz3, end="\t|")
+        print("")
+
+kelas()
+quiz1()
+quiz2()
+quiz3() 
+
